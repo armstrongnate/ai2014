29df73e860c4de81d0e9181f7e030bf42fb536aa
Nate Armstrong <natearmstrong2@gmail.com>
2014-09-21 14:41:58 -0600

    Adds SearchState class. #3

--- ai-agents/prog/ScavengerWorld/AgentPrograms.pm
+++ ai-agents/prog/ScavengerWorld/AgentPrograms.pm
@@ -21,7 +21,7 @@ our $aps =
    name => 'WyWy',
    key  => 'w',
    header => 'naaWyWy.h',
-   cpp    => ['naaWyWy.cpp', 'naaModel.cpp'],
+   cpp    => ['naaWyWy.cpp', 'naaModel.cpp', 'naaState.cpp'],
    constructor => 'naa::WyWy(opts)',
  },
 #{
